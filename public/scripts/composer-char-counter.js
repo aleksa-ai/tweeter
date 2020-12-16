2aa7223a42ef8da1fbb2d1c79ff6656f9db936a6
Aleksa Gavrilovic <lks_g@live.ca>
2020-12-15 23:17:02 -0500

    added comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $("#tweet-text").on('input', function (event) {
+    //get the input value of the text entered in a textarea by its tweet-text id
     console.log(140 - $(this).val().length);
+    //print character left count by substracting to 140 the length i.e. number of character
   })
 }); 
