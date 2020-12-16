ed621c0559c2b2cca9f0eee1316ebd659dd9864b
Aleksa Gavrilovic <lks_g@live.ca>
2020-12-15 23:49:57 -0500

    make 'character left count' traverse into counter to update output

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
   $("#tweet-text").on('input', function (event) {
     //get the input value of the text entered in a textarea by its tweet-text id
-    let charCount = 140 - $(this).val().length;
+    let charLeft = 140 - $(this).val().length;
     //store in variable character left count by substracting to 140 the length i.e. number of character
-    console.log(charCount);
+    $(this)
+      .siblings()
+      .children('.counter')
+      //traversed
+      .text(charLeft);
+      //set text contents of the matched elements
   });
 });
