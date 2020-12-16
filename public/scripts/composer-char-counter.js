e4c594731b5c035e71c75d18888a5d608c80b4b9
Aleksa Gavrilovic <lks_g@live.ca>
2020-12-15 23:24:58 -0500

    variable storage (and printing) of previous step. next step: traverse?

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   $("#tweet-text").on('input', function (event) {
     //get the input value of the text entered in a textarea by its tweet-text id
-    console.log(140 - $(this).val().length);
-    //print character left count by substracting to 140 the length i.e. number of character
-  })
-}); 
+    let charCount = 140 - $(this).val().length;
+    //store in variable character left count by substracting to 140 the length i.e. number of character
+    console.log(charCount);
+  });
+});
