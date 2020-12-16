05437c793f0226991a1af274d3cff779741d22d2
Aleksa Gavrilovic <lks_g@live.ca>
2020-12-16 00:11:17 -0500

    defined counter and updated previous step. set conditions for font colour to change to red when character limit exceeded

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,11 +3,18 @@ $(document).ready(function() {
     //get the input value of the text entered in a textarea by its tweet-text id
     let charLeft = 140 - $(this).val().length;
     //store in variable character left count by substracting to 140 the length i.e. number of character
-    $(this)
-      .siblings()
-      .children('.counter')
-      //traversed
-      .text(charLeft);
-      //set text contents of the matched elements
+    
+    var counter = $(this).siblings().find('.counter')
+    //define counter & traverse into siblings and find element with counter class
+    counter.text(charLeft);
+    //set the text to be equal the value of charLeft
+    
+    //if character limit of 140 exceeded, add class responsible to change font colour to red
+    if (charLeft < 0) {
+      counter.addClass('fontRed');
+    } else {
+      counter.removeClass('fontRed');
+    }
+
   });
 });
